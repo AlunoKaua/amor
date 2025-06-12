d726cfa9bf3a2ba6de81b18b56da82a40eb75ee9
AlunoKaua <164973802+AlunoKaua@users.noreply.github.com>
2025-06-12 18:18:42 -0400

    deploy inicial

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+ import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/amor/', // 👈 isso aqui é ESSENCIAL pro GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
